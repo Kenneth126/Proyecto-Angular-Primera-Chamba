07d94e931277ec5e926a53b88b0f9a462334c587
Kenneth126 <77871436+Kenneth126@users.noreply.github.com>
2023-11-11 10:25:21 -0600

    Primera Chamba
    Esta es la primera versión de nuestra primera chamba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
+    AppComponent,
+    HomeComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ContactoComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'Nosotros', component: NosotrosComponent },
+      {path: 'Home' , component: HomeComponent},
+      {path: 'Servicios', component: ServiciosComponent},
+      {path: 'Contacto', component: ContactoComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+import {
+  Collapse,
+  Carousel,
+  Dropdown,
+  initTE,
+} from "tw-elements";
+
+initTE({ Collapse, Dropdown, Carousel });
